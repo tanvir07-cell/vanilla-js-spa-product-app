112fdd44b4eff0984fea3cec72c6a2f4fad73342
tanvir07-cell <tr9836859@gmail.com>
2024-08-25 01:20:55 -0700

    service worker offline support

--- sw.js
+++ sw.js
@@ -1,3 +1,35 @@
+// offline support :
+
+self.addEventListener("install", async (event) => {
+  const assets = [
+    "/",
+    "./app.js",
+    "style.css",
+    "/services/api.js",
+    "/services/idb.js",
+    "/services/router.js",
+    "/services/state.js",
+    "/components/ProductsPage.js",
+    "/components/ProductsPage.css",
+    "/components/DetailsPage.js",
+    "/components/DetailsPage.css",
+    "/components/OrderPage.js",
+    "/components/OrderPage.css",
+
+    "/utils/addToCart.js",
+    "/utils/getProductById.js",
+    "/utils/loadProducts.js",
+    "/utils/removeProductById.js",
+    "https://cdn.jsdelivr.net/npm/idb@7/build/umd.js",
+    "https://fonts.googleapis.com/css?family=Rancho&effect=shadow-multiple|3d-float|fire-animation|neon|outline|emboss|shadow-multiple|shadow-sweep|shadow-w-multiple|shadow-w-super|shadow-amour|shadow-into-light|shadow-into-light-two|shadow-dancer",
+    "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
+    "https://fonts.gstatic.com/s/rancho/v21/46kulbzmXjLaqZRVam_hVUdI1w.woff2",
+  ];
+  const cache = await caches.open("cm-assets");
+  cache.addAll(assets);
+});
+
+// for acceccisg the caching images
 self.addEventListener("fetch", async (event) => {
   event.respondWith(
     (async () => {
