bde28c9994d8244ba22da5b0ca92b1c17f585756
tanvir07-cell <tr9836859@gmail.com>
2024-08-25 02:22:38 -0700

    Chore:for the responsivenenss

--- sw.js
+++ sw.js
@@ -33,18 +33,12 @@ self.addEventListener("install", async (event) => {
 self.addEventListener("fetch", async (event) => {
   event.respondWith(
     (async () => {
-      const cachedResponse = await caches.match(event.request);
-      if (cachedResponse) {
-        console.log("Returning cached response for:", event.request.url);
-        return cachedResponse;
-      }
       try {
-        const networkResponse = await fetch(event.request);
-        console.log("Network request for:", event.request.url);
-        return networkResponse;
-      } catch (error) {
-        console.error("Fetch failed for:", event.request.url, error);
-        throw error;
+        const fetchResponse = await fetch(event.request);
+        return fetchResponse;
+      } catch (e) {
+        const cachedResponse = await caches.match(event.request);
+        if (cachedResponse) return cachedResponse;
       }
     })()
   );
